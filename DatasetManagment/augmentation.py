ab212360d9938b91d2c0e8850df3da349f2dcc8b
Jankoetf <jankomitrovicetf@gmail.com>
2025-03-26 17:33:56 +0100

    paths automated

--- DatasetManagment/augmentation.py
+++ DatasetManagment/augmentation.py
@@ -1,15 +1,19 @@
 import openai
 import pandas as pd
 import random
+
 from DatasetManagment.keyboard_neighborhood import keyboard_neighborhood
+from DatasetManagment.dataset_const import (TEST_DATASETS_ROOT, DATASETS_ROOT)
 
 class DatasetAugmentation:
-    def __init__(self, keyboard_neighborhood = keyboard_neighborhood, engine = "gpt-4o"):
+    def __init__(self, keyboard_neighborhood = keyboard_neighborhood, engine = "gpt-4o", root_relative_path = TEST_DATASETS_ROOT):
         self.engine = engine
         self.keyboard_neighborhood = keyboard_neighborhood
+        self.root_relative_path = root_relative_path
+        self.sheet_name_default = "All"
         self.prompt_setup()
 
-    def generate_response(self, prompt, temperature = 0.9, n_of_responses = 2):
+    def generate_response(self, prompt, temperature = 0.8, n_of_responses = 1):
         response = openai.ChatCompletion.create(
             model=self.engine,
             messages=[{"role": "user", "content": prompt}],
@@ -21,6 +25,9 @@ class DatasetAugmentation:
 
         responses = [choice['message']['content'].strip() for choice in response['choices']]
         return responses
+    
+    def path_from_name(self, dataset_name):
+        return self.root_relative_path + dataset_name + ".xlsx"
 
     def prompt_setup(self):
         self.promt_intro = "Parafraziraj sledeću rečenicu tako da može zadržati ili promeniti značenje, ali je važno da priroda rečenice "
@@ -39,10 +46,15 @@ class DatasetAugmentation:
         new_prompt = self.promt_intro + self.conditional_part_contex[old_label] + self.prompt_middle + self.conditional_part_example[old_label] + f"\nRečenica: {old_text}"
         return new_prompt
 
-    def new_dataset_by_paraphrazing(self, original_dataset, new_dataset_name):
+    def new_dataset_by_paraphrazing(self, original_dataset, new_dataset_name, hyperparams = None):
         texts, labels = [], []
-        n_responses_per_request = 2
-        list_of_temperatures = [0.75, 0.85, 0.95]
+        if hyperparams == None:
+            n_responses_per_request = 2
+            list_of_temperatures = [0.75, 0.85, 0.95]
+        else:
+            n_responses_per_request = hyperparams["n_responses_per_request"]
+            list_of_temperatures = hyperparams["list_of_temperatures"]
+
         n_of_temperatures = len(list_of_temperatures)
         augmentation = n_responses_per_request*n_of_temperatures
 
@@ -61,7 +73,7 @@ class DatasetAugmentation:
           'label': labels
         })
 
-        df.to_excel(new_dataset_name + ".xlsx", sheet_name="All", index=False)
+        df.to_excel(self.path_from_name(new_dataset_name), sheet_name=self.sheet_name_default, index=False)
 
     def add_typo(self, original_text, typo_prob = 0.03):
         typos_count = 0
@@ -109,4 +121,9 @@ class DatasetAugmentation:
           'label': labels
         })
 
-        df.to_excel(new_file_name + ".xlsx", sheet_name="All", index=False)
+        df.to_excel(self.path_from_name(new_file_name), sheet_name=self.sheet_name_default, index=False)
+
+    def filter_duplicates(self, original_dataset_path, new_file_name):
+        df = pd.read_excel(original_dataset_path, sheet_name = self.sheet_name_default)
+        df_cleaned = df.drop_duplicates(subset=['text'])
+        df_cleaned.to_excel(self.path_from_name(new_file_name), sheet_name = self.sheet_name_default, index=False)
